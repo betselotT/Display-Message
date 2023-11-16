497a214b54b80259afe9ae9e036ead44d822e303
Betselot Tesfa <betselottesfa9@gmail.com>
2023-11-16 10:34:25 -0900

    added a reveal function

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 (function() {
+    let eyeicon = document.querySelector(".eyeicon")
+    let password = document.querySelector(".password")
     const form = document.querySelector("#message-form")
+    const header = document.querySelector(".temp")
+    header.style.display = 'none'
     form.addEventListener('submit', function(e) {
         e.preventDefault()
 
-        const message = document.querySelector("#message")
+        const message = document.querySelector(".message")
         const feedback = document.querySelector(".feedback")
         const messageContent = document.querySelector(".message-content")
     
@@ -15,6 +19,17 @@
         } else{
             messageContent.textContent = message.value
             message.value = ''
+            header.style.display = 'block'
         }
     })
+
+    eyeicon.onclick = function(){
+        if(password.type == "password"){
+            password.type = "text";
+            eyeicon.src = "eye-open.png";
+        } else{
+            password.type = "password";
+            eyeicon.src = "eye-close.png";
+        }
+    }
 })()
